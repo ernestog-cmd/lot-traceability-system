a4a6984c3018cf70d35f6de088b503344a198093
Gerardo <ernestog.arreola@icloud.com>
2026-08-02 21:15:54 -0700

    Add create lot form with success and error feedback

--- static/app.js
+++ static/app.js
@@ -23,3 +23,33 @@ async function loadLots() {
     }
 }
 loadLots();
+
+const form = document.getElementById("create-lot-form");
+form.addEventListener("submit", async function (event) {
+    event.preventDefault();
+
+    const newLot = {
+        id: document.getElementById("id").value,
+        part_number: document.getElementById("part_number").value,
+        description: document.getElementById("description").value,
+        product_family: document.getElementById("product_family").value,
+        units: parseInt(document.getElementById("units").value),
+        manufacturing_date: document.getElementById("manufacturing_date").value,
+        status: "ready_for_audit"
+    };
+    
+    const response = await fetch("/lots", {
+        method: "POST",
+        headers: {"Content-Type": "application/json" },
+        body: JSON.stringify(newLot)
+    });
+
+    if (response.ok) {
+        const createdLot = await response.json();
+        form.reset();
+        loadLots();
+        alert(`Lot ${createdLot.id} created successfully`);
+    } else {
+        alert("Error creating lot")
+    }
+});
